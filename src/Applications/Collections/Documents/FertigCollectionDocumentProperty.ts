29cd773f7e188d7cff7b37df58937b22621f097f
Jan Jaap <mail@janjaap.de>
2021-07-07 21:53:41 +0200

    fix: refactoring broke stuff

--- src/Applications/Collections/Documents/FertigCollectionDocumentProperty.ts
+++ src/Applications/Collections/Documents/FertigCollectionDocumentProperty.ts
@@ -1,9 +1,9 @@
-import {FertigCollectionColumn} from '../FertigCollectionColumn';
+import {FerdigApplicationCollectionColumn} from '../FerdigApplicationCollectionColumn';
 
 export class FertigCollectionDocumentProperty {
     public id: string;
     public createdAt: Date;
     public updateAt: Date;
-    public column: FertigCollectionColumn;
+    public column: FerdigApplicationCollectionColumn;
     public value: string;
 }
